68de1c4e2c9b040d55e3ba00133f3fd9ffc0c1d9
Lukas Karlsson <karlsson@broadinstitute.org>
2022-05-06 16:35:15 -0400

    Cleaned up bash scripts.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 PROJECT="broad-bitstore-app"
-if [ "$1" == 'dev' ]; then
+if [ "$1" == "dev" ]; then
   PROJECT="broad-bitstore-app-dev"
-elif [ "$1" == 'sandbox' ]; then
+elif [ "$1" == "sandbox" ]; then
   PROJECT="broad-bitstore-app-sandbox"
 fi
 
 # deploy app
-gcloud -q app deploy --project ${PROJECT}
+gcloud -q app deploy --project "${PROJECT}"
--- develop.sh
+++ develop.sh
@@ -2,27 +2,27 @@
 
 export PATH=/opt/google-cloud-sdk/bin:$PATH
 
-APP='broad-bitstore-app'
-SERVICEACCOUNT='service_account_prod.json'
-if [ "$1" == 'dev' ]; then
+APP="broad-bitstore-app"
+SERVICEACCOUNT="service_account_prod.json"
+if [ "$1" == "dev" ]; then
   APP="broad-bitstore-app-dev"
-  SERVICEACCOUNT='service_account_dev.json'
-elif [ "$1" == 'sandbox' ]; then
+  SERVICEACCOUNT="service_account_dev.json"
+elif [ "$1" == "sandbox" ]; then
   APP="broad-bitstore-app-sandbox"
-  SERVICEACCOUNT='service_account_sandbox.json'
+  SERVICEACCOUNT="service_account_sandbox.json"
 fi
 
 IMAGE="gcr.io/${APP}/bitstore-app:latest"
 
 # pull the newest image
-docker pull ${IMAGE}
+docker pull "${IMAGE}"
 
 # run the container
 docker run -it --rm \
   --expose 8080 \
   -e GOOGLE_APPLICATION_CREDENTIALS=/usr/src/etc/${SERVICEACCOUNT} \
   -p 8080:8080 \
-  -v `pwd`:/usr/src \
+  -v "$(pwd)":/usr/src \
   -w /usr/src \
-  ${IMAGE} \
+  "${IMAGE}" \
   python main.py
